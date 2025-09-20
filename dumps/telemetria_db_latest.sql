895b341ae5b7138a7966ce5402fd27dc8dc11cfe
Lucas Farre <lucas.farre@buonnytech.com.br>
2025-09-20 11:40:27 -0300

    dump(telemetria_db): 20250920_114024

--- dumps/telemetria_db_latest.sql
+++ dumps/telemetria_db_latest.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:349739ec411a550d905b1e5d612f8d50ea9f45e4b758f4cf9474e9b7d9474621
-size 145807983
+oid sha256:6af66c7e8a7a524d2615dd4b6c577df8b1f6352dcc41dc6fad711a27f3ac1ec5
+size 146408511
